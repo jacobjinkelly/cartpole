b4c33f8809b678f188c753587b24c98ffa06826c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-04-12 17:34:55 -0400

    train

---
+++ train.py
@@ -0,0 +1,16 @@
+"""This module contains methods for training agents.
+"""
+
+from agent import Agent
+import gym
+
+def random(agent: Agent) -> Agent:
+    """Initialize 10,000 agents randomly, and pick the best one.
+    """
+    pass
+
+def hill_climb(agent: Agent) -> Agent:
+    """Initialize an agent randomly, and randomly pertube the weights. If the
+    random pertubation achieves better performance, update the weights.
+    """
+    pass
