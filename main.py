58b949bfd218b3c2fa5dcd7bcc2b863b134e03f0
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-04-16 22:27:26 -0400

    removed unnecessary env creation in main

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import train
 gym.logger.set_level(40) # set level to ERROR, i.e. so doesn't log WARN level
 # (in particular so it doesn't WARN about automatic detecting of dtype)
 
-env = gym.make('CartPole-v1')
-observation = env.reset()
-
 agent = train.reinforce(StochasticAgent())
 
 print(train.avg_reward(agent))
