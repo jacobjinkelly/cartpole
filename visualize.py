7ffac854175f54cc5d3d673e47641390115661ab
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-06-26 19:22:58 -0400

    add plotting

--- visualize.py
+++ visualize.py
@@ -2,4 +2,21 @@
 This module renders visualizations of statistics obtained from experiment.py
 """
 
+import os
+
 import matplotlib.pyplot as plt
+
+
+def show_plot(points, save: bool=False, name: str="plot", xlabel: str="", ylabel: str=""):
+    """
+    Show a scatter plot of the given points.
+    """
+    x, y = zip(points)
+
+    plt.xticks(x), plt.xlabel(xlabel), plt.ylabel(ylabel)  # formatting
+    plt.plot(x, y)
+    if save:
+        if not os.path.isdir("imgs"):
+            os.mkdir("imgs")
+        plt.savefig("imgs/" + name + ".png")
+    plt.show()
