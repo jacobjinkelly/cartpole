542a21fcefe27ba0d80f1e004ac1e3c747e137a0
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-06-27 18:04:11 -0400

    refactor and doc

--- experiment.py
+++ experiment.py
@@ -2,7 +2,6 @@
 This module records statistics of the agents' performance.
 """
 
-import csv
 from typing import List
 
 import train
@@ -14,14 +13,14 @@ def random_num_trials(nums_trials: List[int], num_samples: int):
     Comparing number of trials to performance of "random" training algorithm at finding agent which solves environment.
     """
     results = []
-    for i in range(len(nums_trials)):
-        for j in range(num_samples):
-            print("num_trials: %d, sample: %d" % (nums_trials[i], j + 1))
-            results.append((nums_trials[i], train.get_avg_reward(agent=train.random(population=10000,
-                                                                                    num_trials=nums_trials[i],
-                                                                                    mean=0,
-                                                                                    std_dev=1),
-                                                                 num_trials=100)))
+    for num_trials in nums_trials:
+        for i in range(num_samples):
+            print("num_trials: %d, sample: %d" % (num_trials, i + 1))
+            results.append((num_trials, train.get_avg_reward(agent=train.random(population=10000,
+                                                                                num_trials=num_trials,
+                                                                                mean=0,
+                                                                                std_dev=1),
+                                                             num_trials=100)))
     show_plot(results, save=True, name="random_trial_length", xlabel="Number of Trials",
               ylabel="Avg. Reward over 100 trials")
 
@@ -32,75 +31,62 @@ def hill_climb_trial_length(nums_trials: List[int], num_samples: int):
     environment.
     """
     results = []
-    for i in range(len(nums_trials)):
-        for j in range(num_samples):
-            print("num_trials: %d, sample: %d" % (nums_trials[i], j + 1))
-            agent, _ = train.hill_climb(num_trials=nums_trials[i],
+    for num_trials in nums_trials:
+        for i in range(num_samples):
+            print("num_trials: %d, sample: %d" % (num_trials, i + 1))
+            agent, _ = train.hill_climb(num_trials=num_trials,
                                         mean=0,
                                         std_dev=1,
                                         max_reward=200)
-            results.append((nums_trials[i], train.get_avg_reward(agent=agent, num_trials=100)))
+            results.append((num_trials, train.get_avg_reward(agent=agent, num_trials=100)))
     show_plot(results, save=True, name="hill_climb_trial_length", xlabel="Number of Trials",
               ylabel="Avg. Reward over 100 trials")
 
 
 def hill_climb_std_dev(std_devs: List[int], num_samples: int):
     """
-    Tuning std_dev hyperparameter of "hill climbing" algorithm.
+    Comparing standard deviation of distribution from which weight pertubation is sampled from to performance of hill
+    climbing training algorithm at finding agent which solves environment.
     """
     results_std_dev, results_trajectories = [], {std_dev: [] for std_dev in std_devs}
-    for i in range(len(std_devs)):
-        for j in range(num_samples):
-            print("std_dev: %d, sample: %d" % (std_devs[i], j + 1))
+    for std_dev in std_devs:
+        for i in range(num_samples):
+            print("std_dev: %d, sample: %d" % (std_dev, i + 1))
             agent, trajectory = train.hill_climb(num_trials=5,
                                                  mean=0,
-                                                 std_dev=std_devs[i],
+                                                 std_dev=std_dev,
                                                  max_reward=200)
-            results_trajectories[std_devs[i]].append(list(trajectory))
-            results_std_dev.append((std_devs[i], train.get_avg_reward(agent=agent, num_trials=100)))
+            results_trajectories[std_dev].append(trajectory)
+            results_std_dev.append((std_dev, train.get_avg_reward(agent=agent, num_trials=100)))
 
 
-def reinforce_alpha():
+def reinforce_lr(lrs: List[float], num_samples: int):
     """
-    Tuning step size of reinforce algorithm.
+    Comparing learning rate to performance of REINFORCE algorithm at finding agent which solves environment.
     """
-    vals = [0.0001, 0.001, 0.01, 0.1]
-    with open("results/reinforce_alpha.csv", "w", newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ', quotechar='|',
-                            quoting=csv.QUOTE_MINIMAL)
-        for i in range(len(vals)):
-            alpha = vals[i]
-            writer.writerow([str(alpha)])
-            for j in range(1):
-                print(alpha, j)
-                agent, q = train.reinforce(alpha, 5, 200, 200)
-                while True:
-                    try:
-                        t, reward = q.popleft()
-                        writer.writerow((str(t), str(reward)))
-                    except IndexError:
-                        break
-                writer.writerow([str(train.get_avg_reward(agent, 100))])
+    results_lr, results_trajectories = [], {lr: [] for lr in lrs}
+    for lr in lrs:
+        for i in range(num_samples):
+            print("learning rate: %s, sample: %d" % (lr, i + 1))
+            agent, trajectory = train.reinforce(lr=lr,
+                                                num_rollouts=5,
+                                                horizon=200,
+                                                max_reward=200)
+            results_trajectories[lr].append(trajectory)
+            results_lr.append((lr, train.get_avg_reward(agent=agent, num_trials=100)))
 
 
-def reinforce_rollouts():
+def reinforce_rollouts(nums_rollouts: List[int], num_samples: int):
     """
-    Tune number of rollouts.
+    Comparing number of rollouts to performance of REINFORCE algorithm at finding agent which solves environment.
     """
-    vals = [10, 25, 50, 100]
-    with open("results/reinforce_rollouts.csv", "w", newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ', quotechar='|',
-                            quoting=csv.QUOTE_MINIMAL)
-        for i in range(len(vals)):
-            num_rollouts = vals[i]
-            writer.writerow([str(num_rollouts)])
-            for j in range(1):
-                print(num_rollouts, j)
-                agent, q = train.reinforce(0.0001, num_rollouts, 200, 200)
-                while True:
-                    try:
-                        t, reward = q.popleft()
-                        writer.writerow((str(t), str(reward)))
-                    except IndexError:
-                        break
-                writer.writerow([str(train.get_avg_reward(agent, 100))])
+    results_rollouts, results_trajectories = [], {num_rollouts: [] for num_rollouts in nums_rollouts}
+    for num_rollouts in nums_rollouts:
+        for i in range(num_samples):
+            print("num_rollouts: %d, sample: %d" % (num_rollouts, i + 1))
+            agent, trajectory = train.reinforce(lr=0.0001,
+                                                num_rollouts=num_rollouts,
+                                                horizon=200,
+                                                max_reward=200)
+            results_trajectories[num_rollouts].append(trajectory)
+            results_rollouts.append((num_rollouts, train.get_avg_reward(agent=agent, num_trials=100)))
