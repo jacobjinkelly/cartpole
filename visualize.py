b658d73e4ebce4839c59d587e04488f61042f14b
Jacob Kelly <jacob.jin.kelly@gmail.com>
2018-07-10 21:36:16 -0400

    add frequency histogram

--- visualize.py
+++ visualize.py
@@ -2,13 +2,13 @@
 This module renders visualizations of statistics obtained from experiment.py
 """
 import os
-from typing import Callable, Union, Optional, Tuple
+from typing import Callable, List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def show_plot(points, save: bool=False, name: str="plot", xlabel: str="", ylabel: str=""):
+def show_plot(points: List[Tuple[int, float]], save: bool=False, name: str="plot", xlabel: str="", ylabel: str=""):
     """
     Show a scatter plot of the given points.
     """
@@ -23,7 +23,21 @@ def show_plot(points, save: bool=False, name: str="plot", xlabel: str="", ylabel
     plt.show()
 
 
-def draw_function(f: Callable[[np.ndarray], float], save=True, name="", clear_plot=True):
+def show_freq_hist(vals: List[int], save: bool=False, name: str="plot", xlabel: str="", ylabel: str=""):
+    """
+    Plots a (normalized) frequency histogram
+    """
+    plt.xticks([i for i in range(max(vals) + 1)]), plt.xlabel(xlabel), plt.ylabel(ylabel)  # formatting
+    # credit to https://stackoverflow.com/questions/9767241/setting-a-relative-frequency-in-a-matplotlib-histogram
+    plt.hist(vals, weights=np.zeros_like(vals) + 1 / len(vals))
+    if save:
+        if not os.path.isdir("imgs"):
+            os.mkdir("imgs")
+        plt.savefig("imgs/" + name + ".png")
+    plt.show()
+
+
+def draw_function(f: Callable[[np.ndarray], float], save: bool=True, name: str="", clear_plot: bool=True):
     """
     Plot a smooth curve of the 1D function <f>.
     """
